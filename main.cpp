24c4cbe78a429a9f74b0150c5ceacf9f4cb2019b
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-06-30 20:58:24 +0300

    ex add

--- main.cpp
+++ main.cpp
@@ -2,9 +2,32 @@
 #include <Windows.h>
 using namespace std;
 
+int number(string binar) {
+    int value = 0;
+    int base = 1;
+
+    for (int i = binar.length() - 1; i >= 0; i--) {
+        if (binar[i] == '1') {
+            value += base;
+        }
+        base *= 2;
+    }
+
+    return value;
+}
+
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+
+    string binar;
+
+    cout << "Введіть двійкове число: ";
+    cin >> binar;
+
+    int value = number(binar);
+
+    cout << "Десяткове подання: " << value << endl;
 
     return 0;
 }
